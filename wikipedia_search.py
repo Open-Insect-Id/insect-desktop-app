740652cda37fdcb97b2d00a86c3e179833a38339
Elnix90 <elnix91@proton.me>
2026-02-26 19:13:22 +0100

    ajout recherche wikipedia
    les pages wikipedia des espèces peuvent etre ouvertes avec un nouveau bouton a coté du bouton carte

---
+++ wikipedia_search.py
@@ -0,0 +1,30 @@
+"""
+Module qui s'occupe des fonctions en lien avec Wikipedia
+"""
+
+import wikipedia
+import webbrowser
+
+
+def summary(text):
+    wikipedia.set_lang("fr")
+    try:
+        summary = wikipedia.summary(text, sentences = 10)
+    except wikipedia.DisambiguationError as error:
+        summary = wikipedia.summary(error.options[0], sentences = 10)
+    except wikipedia.PageError as error:
+        wikipedia.set_lang("en")
+        summary = wikipedia.summary(text, sentences = 10)
+
+    print(summary)
+
+def search(text):
+    lang = "fr"
+    wikipedia.set_lang("fr")
+    search = wikipedia.search(text, results = 1, suggestion=False)
+    if search == []:
+        lang="en"
+        wikipedia.set_lang("en")
+        search = wikipedia.search(text, results = 1, suggestion=False)
+
+    webbrowser.open("https://"+lang+".wikipedia.com/wiki/"+search[0])
